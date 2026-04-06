4f2776bbd4e331fc4b1da5cdac2292fc08e1aec9
midhila345 <midhilanair1.1@gmail.com>
2026-04-06 19:14:09 +0530

    updated file

--- script.js
+++ script.js
@@ -1,23 +1,81 @@
-// SELECT ELEMENTS
-const cards = document.querySelectorAll(".card img");
+const cards = document.querySelectorAll(".card");
 const viewer = document.getElementById("viewer");
-const viewerImg = document.getElementById("viewer-img");
+const viewerContent = document.getElementById("viewer-content");
 const closeBtn = document.querySelector(".close");
 
-// OPEN VIEWER
+// PROJECT IMAGE DATA
+const projects = {
+  brochure: [
+    "images/brochure/brochure1.JPG",
+    "images/brochure/brochure2.JPG"
+  ],
+  branding: [
+    "images/branding/branding1.jpg",
+    "images/branding/branding2.jpg",
+    "images/branding/branding3.jpg",
+    "images/branding/branding4.jpg",
+    "images/branding/branding5.jpg",
+    "images/branding/branding6.jpg",
+    "images/branding/branding7.jpg",
+    "images/branding/branding8.jpg",
+    "images/branding/branding9.jpg",
+    "images/branding/branding10.jpg",
+    "images/branding/branding11.jpg",
+    "images/branding/branding12.jpg",
+    "images/branding/branding13.jpg",
+    "images/branding/branding14.jpg",
+    "images/branding/branding15.jpg"
+  ],
+  posters: [
+    "images/social-media/CUPP3.png",
+    "images/social-media/HEADF.png",
+    "images/social-media/drink.png"
+  ],
+  "3d": [
+    "images/3d-models/model1.jpg",
+    "images/3d-models/model2.jpg",
+    "images/3d-models/model3.jpg",
+    "images/3d-models/model4.jpg",
+    "images/3d-models/model5.jpg",
+    "images/3d-models/model6.jpg"
+  ],
+  ui: [
+    "images/UI Design/ui1.jpg",
+    "images/UI Design/ui2.jpg",
+    "images/UI Design/ui3.jpg",
+    "images/UI Design/ui4.jpg",
+    "images/UI Design/ui5.jpg",
+    "images/UI Design/ui6.jpg",
+    "images/UI Design/ui7.jpg",
+    "images/UI Design/ui8.jpg",
+    "images/UI Design/ui9.jpg",
+    "images/UI Design/ui10.jpg"
+  ]
+};
+
+// CLICK CARD
 cards.forEach(card => {
   card.addEventListener("click", () => {
-    viewer.style.display = "flex";
-    viewerImg.src = card.src;
+    const project = card.getAttribute("data-project");
+
+    viewerContent.innerHTML = ""; // clear old
+
+    projects[project].forEach(src => {
+      const img = document.createElement("img");
+      img.src = src;
+      viewerContent.appendChild(img);
+    });
+
+    viewer.style.display = "block";
   });
 });
 
-// CLOSE VIEWER (click X)
+// CLOSE
 closeBtn.addEventListener("click", () => {
   viewer.style.display = "none";
 });
 
-// CLOSE VIEWER (click outside image)
+// CLOSE on background click
 viewer.addEventListener("click", (e) => {
   if (e.target === viewer) {
     viewer.style.display = "none";
